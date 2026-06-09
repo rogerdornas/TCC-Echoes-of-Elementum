83e6eb2859842b2770c6b25cdf4fab33f7c561e8
rogerdornas <rogerdornas@gmail.com>
2026-06-09 02:58:25 -0300

    add better resolution brown cave decorations

---
+++ Assets/Sprites/BrownCave2/BrownCaveDecorations.tsx
@@ -0,0 +1,307 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="BrownCaveDecorations" tilewidth="1502" tileheight="769" tilecount="101" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="BigRock01.png" width="741" height="581"/>
+ </tile>
+ <tile id="1">
+  <image source="BigRock02.png" width="393" height="261"/>
+ </tile>
+ <tile id="2">
+  <image source="BigRock03.png" width="586" height="257"/>
+ </tile>
+ <tile id="3">
+  <image source="BigRock04.png" width="323" height="510"/>
+ </tile>
+ <tile id="4">
+  <image source="BigRock05.png" width="257" height="213"/>
+ </tile>
+ <tile id="5">
+  <image source="BigRock06.png" width="354" height="395"/>
+ </tile>
+ <tile id="6">
+  <image source="BigRock07.png" width="697" height="395"/>
+ </tile>
+ <tile id="7">
+  <image source="BigRock08.png" width="610" height="391"/>
+ </tile>
+ <tile id="8">
+  <image source="BigRock09.png" width="248" height="769"/>
+ </tile>
+ <tile id="9">
+  <image source="Floor01.png" width="381" height="89"/>
+ </tile>
+ <tile id="10">
+  <image source="Floor02.png" width="396" height="93"/>
+ </tile>
+ <tile id="11">
+  <image source="Floor03.png" width="162" height="77"/>
+ </tile>
+ <tile id="12">
+  <image source="Floor04.png" width="374" height="84"/>
+ </tile>
+ <tile id="13">
+  <image source="Floor05.png" width="393" height="86"/>
+ </tile>
+ <tile id="14">
+  <image source="Floor06.png" width="434" height="151"/>
+ </tile>
+ <tile id="15">
+  <image source="Floor07.png" width="1017" height="108"/>
+ </tile>
+ <tile id="16">
+  <image source="Floor08.png" width="812" height="100"/>
+ </tile>
+ <tile id="17">
+  <image source="Floor09.png" width="311" height="92"/>
+ </tile>
+ <tile id="18">
+  <image source="Floor10.png" width="320" height="93"/>
+ </tile>
+ <tile id="19">
+  <image source="Floor11.png" width="248" height="92"/>
+ </tile>
+ <tile id="20">
+  <image source="Floor12.png" width="241" height="100"/>
+ </tile>
+ <tile id="21">
+  <image source="Floor13.png" width="229" height="362"/>
+ </tile>
+ <tile id="22">
+  <image source="Floor14.png" width="229" height="363"/>
+ </tile>
+ <tile id="23">
+  <image source="Floor15.png" width="375" height="240"/>
+ </tile>
+ <tile id="24">
+  <image source="Floor16.png" width="369" height="235"/>
+ </tile>
+ <tile id="25">
+  <image source="Floor17.png" width="180" height="407"/>
+ </tile>
+ <tile id="26">
+  <image source="Floor18.png" width="141" height="408"/>
+ </tile>
+ <tile id="27">
+  <image source="Floor19.png" width="420" height="152"/>
+ </tile>
+ <tile id="28">
+  <image source="Floor20.png" width="410" height="136"/>
+ </tile>
+ <tile id="29">
+  <image source="Floor21.png" width="1502" height="191"/>
+ </tile>
+ <tile id="30">
+  <image source="Floor22.png" width="1475" height="304"/>
+ </tile>
+ <tile id="31">
+  <image source="Floor23.png" width="751" height="245"/>
+ </tile>
+ <tile id="32">
+  <image source="Floor24.png" width="718" height="203"/>
+ </tile>
+ <tile id="33">
+  <image source="Floor25.png" width="815" height="271"/>
+ </tile>
+ <tile id="34">
+  <image source="Floor26.png" width="811" height="334"/>
+ </tile>
+ <tile id="35">
+  <image source="Platform01.png" width="210" height="378"/>
+ </tile>
+ <tile id="36">
+  <image source="Platform02.png" width="318" height="74"/>
+ </tile>
+ <tile id="37">
+  <image source="Platform03.png" width="280" height="168"/>
+ </tile>
+ <tile id="38">
+  <image source="Platform04.png" width="403" height="119"/>
+ </tile>
+ <tile id="39">
+  <image source="Platform05.png" width="375" height="82"/>
+ </tile>
+ <tile id="40">
+  <image source="Platform06.png" width="207" height="114"/>
+ </tile>
+ <tile id="41">
+  <image source="Platform07.png" width="206" height="202"/>
+ </tile>
+ <tile id="42">
+  <image source="Platform08.png" width="401" height="200"/>
+ </tile>
+ <tile id="43">
+  <image source="Platform09.png" width="331" height="337"/>
+ </tile>
+ <tile id="44">
+  <image source="Platform10.png" width="579" height="375"/>
+ </tile>
+ <tile id="45">
+  <image source="Platform11.png" width="404" height="375"/>
+ </tile>
+ <tile id="46">
+  <image source="RockCombination01.png" width="332" height="281"/>
+ </tile>
+ <tile id="47">
+  <image source="RockCombination02.png" width="410" height="264"/>
+ </tile>
+ <tile id="48">
+  <image source="RockCombination03.png" width="449" height="276"/>
+ </tile>
+ <tile id="49">
+  <image source="RockCombination04.png" width="264" height="261"/>
+ </tile>
+ <tile id="50">
+  <image source="RockCombination05.png" width="266" height="222"/>
+ </tile>
+ <tile id="51">
+  <image source="RockCombination06.png" width="319" height="282"/>
+ </tile>
+ <tile id="52">
+  <image source="RockCombination07.png" width="467" height="423"/>
+ </tile>
+ <tile id="53">
+  <image source="RockCombination08.png" width="603" height="370"/>
+ </tile>
+ <tile id="54">
+  <image source="RockCombination09.png" width="592" height="307"/>
+ </tile>
+ <tile id="55">
+  <image source="RockCombination10.png" width="599" height="398"/>
+ </tile>
+ <tile id="56">
+  <image source="RockCombination11.png" width="732" height="403"/>
+ </tile>
+ <tile id="57">
+  <image source="RockCombination12.png" width="493" height="380"/>
+ </tile>
+ <tile id="58">
+  <image source="RockCombination13.png" width="624" height="632"/>
+ </tile>
+ <tile id="59">
+  <image source="RockCombination14.png" width="449" height="687"/>
+ </tile>
+ <tile id="60">
+  <image source="RockCombination15.png" width="387" height="673"/>
+ </tile>
+ <tile id="61">
+  <image source="RockCombination16.png" width="420" height="650"/>
+ </tile>
+ <tile id="62">
+  <image source="SmallRock01.png" width="172" height="202"/>
+ </tile>
+ <tile id="63">
+  <image source="SmallRock02.png" width="146" height="190"/>
+ </tile>
+ <tile id="64">
+  <image source="SmallRock03.png" width="156" height="190"/>
+ </tile>
+ <tile id="65">
+  <image source="SmallRock04.png" width="157" height="134"/>
+ </tile>
+ <tile id="66">
+  <image source="SmallRock05.png" width="162" height="187"/>
+ </tile>
+ <tile id="67">
+  <image source="SmallRock06.png" width="231" height="197"/>
+ </tile>
+ <tile id="68">
+  <image source="SmallRock07.png" width="217" height="293"/>
+ </tile>
+ <tile id="69">
+  <image source="SmallRock08.png" width="205" height="283"/>
+ </tile>
+ <tile id="70">
+  <image source="SmallRock09.png" width="218" height="293"/>
+ </tile>
+ <tile id="71">
+  <image source="SmallRock10.png" width="166" height="147"/>
+ </tile>
+ <tile id="72">
+  <image source="SmallRock11.png" width="157" height="112"/>
+ </tile>
+ <tile id="73">
+  <image source="SmallRock12.png" width="148" height="96"/>
+ </tile>
+ <tile id="74">
+  <image source="SmallRock13.png" width="203" height="167"/>
+ </tile>
+ <tile id="75">
+  <image source="SmallRock14.png" width="193" height="192"/>
+ </tile>
+ <tile id="76">
+  <image source="SmallRock15.png" width="210" height="170"/>
+ </tile>
+ <tile id="77">
+  <image source="SmallRock16.png" width="136" height="94"/>
+ </tile>
+ <tile id="78">
+  <image source="SmallRock17.png" width="118" height="108"/>
+ </tile>
+ <tile id="79">
+  <image source="SmallRock18.png" width="147" height="173"/>
+ </tile>
+ <tile id="80">
+  <image source="SmallRock19.png" width="188" height="144"/>
+ </tile>
+ <tile id="81">
+  <image source="SmallRock20.png" width="210" height="183"/>
+ </tile>
+ <tile id="82">
+  <image source="SmallRock21.png" width="201" height="173"/>
+ </tile>
+ <tile id="83">
+  <image source="SmallRock22.png" width="410" height="198"/>
+ </tile>
+ <tile id="84">
+  <image source="SmallRock23.png" width="342" height="290"/>
+ </tile>
+ <tile id="85">
+  <image source="SmallRock24.png" width="761" height="594"/>
+ </tile>
+ <tile id="86">
+  <image source="SmallRock25.png" width="762" height="437"/>
+ </tile>
+ <tile id="87">
+  <image source="SmallRock26.png" width="385" height="410"/>
+ </tile>
+ <tile id="88">
+  <image source="SmallRock27.png" width="411" height="275"/>
+ </tile>
+ <tile id="89">
+  <image source="SmallRock28.png" width="346" height="353"/>
+ </tile>
+ <tile id="90">
+  <image source="SmallRock29.png" width="398" height="386"/>
+ </tile>
+ <tile id="91">
+  <image source="SmallRock30.png" width="172" height="366"/>
+ </tile>
+ <tile id="92">
+  <image source="SmallRock31.png" width="197" height="276"/>
+ </tile>
+ <tile id="93">
+  <image source="SmallRock32.png" width="198" height="176"/>
+ </tile>
+ <tile id="94">
+  <image source="SmallRock33.png" width="144" height="199"/>
+ </tile>
+ <tile id="95">
+  <image source="SmallRock34.png" width="334" height="162"/>
+ </tile>
+ <tile id="96">
+  <image source="SmallRock35.png" width="210" height="256"/>
+ </tile>
+ <tile id="97">
+  <image source="SmallRock36.png" width="210" height="398"/>
+ </tile>
+ <tile id="98">
+  <image source="SmallRock37.png" width="210" height="607"/>
+ </tile>
+ <tile id="99">
+  <image source="SmallRock38.png" width="216" height="396"/>
+ </tile>
+ <tile id="100">
+  <image source="SquareBlack.png" width="64" height="64"/>
+ </tile>
+</tileset>
