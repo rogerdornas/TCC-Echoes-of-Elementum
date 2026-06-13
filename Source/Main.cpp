e5dd501eaece7b3e4f8dabe63e70c34bccb00514
rogerdornas <rogerdornas@gmail.com>
2026-06-13 12:31:29 -0300

    add new levels

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -10,6 +10,16 @@
 
 #include "Game.h"
 
+#ifdef _WIN32
+extern "C" {
+    // Sinaliza para o driver da NVIDIA usar a GPU de alta performance
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+
+    // Sinaliza para o driver da AMD usar a GPU de alta performance
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 //Screen dimension constants
 
 // const int SCREEN_WIDTH = 1920;
