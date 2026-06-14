b3fc3677bd9ed5f7101270fd7649c344ffef133f
rogerdornas <rogerdornas@gmail.com>
2026-06-14 15:49:43 -0300

    update world map

--- Source/Actors/Player.cpp
+++ Source/Actors/Player.cpp
@@ -364,7 +364,7 @@ Player::Player(Game* game)
     mDashComponent = new DashComponent(this, mLightningDashSpeed, mLightningDashDuration, mLightningDashCooldown);
 
     mCombatBoxComponent = new CombatBoxComponent(this);
-    mCombatBoxComponent->AddAABBBox("body", false, v1, v3);
+    // mCombatBoxComponent->AddAABBBox("body", false, v1, v3);
     mCombatBoxComponent->AddAABBBox("lightningDash", true, Vector2(-80, -40), Vector2(80, 40));
     mCombatBoxComponent->AddAABBBox("groundSlam", true, Vector2(-35, -60), Vector2(35, 60));
     // mCombatBoxComponent->SetDebugDraw(true);
